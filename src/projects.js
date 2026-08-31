e0a417ce8cdf08edee65233acde92e7a57503ee9
Chirosree007 <chiroshree1999@gmail.com>
2026-08-31 21:19:27 +0530

    Replace todo with serenity yoga studio

--- src/projects.js
+++ src/projects.js
@@ -1,34 +1,31 @@
-
 export const projects = [
+  {
+    title: "PawHome",
+    description:
+      "A pet-focused web interface showcasing adoption details with clean design and usability",
+    tech: "React, Tailwind CSS",
+    image: "/pawhome.png",
+    code: "https://github.com/Chirosree007/Pawhome",
+    live: "https://pawhome-adoption.netlify.app",
+  },
 
-   {
-    title : "PawHome",
-    description : "A pet-focused web interface showcasing adoption details with clean design and usability",
-    tech : "React, Tailwind CSS",
-    image : "/pawhome.png",
-    code : "https://github.com/Chirosree007/Pawhome",
-    live: "https://pawhome-adoption.netlify.app"
-   },
+  {
+    title: "Personal Portfolio",
+    description:
+      "A responsive personal portfolio highlighting projects, skills, and modern frontend practices",
+    tech: "React, Tailwind CSS",
+    image: "/portfolio.png",
+    code: "https://github.com/Chirosree007/Personal-Portfolio",
+    live: "https://chirosree-portfolio.netlify.app",
+  },
 
-   {
-      title : "Personal Portfolio",
-      description: "A responsive personal portfolio highlighting projects, skills, and modern frontend practices",
-      tech : "React, Tailwind CSS",
-      image :"/portfolio.png",
-      code : "https://github.com/Chirosree007/Personal-Portfolio",
-      live: "https://chirosree-portfolio.netlify.app"
-   },
-
-   {
-      title : "To Do List",
-      description: "A simple task manager supporting add, delete, and persistent storage functionality.",
-      tech : "HTML, Vanilla CSS, JS",
-      image :"/todo.png",
-      code : "https://github.com/Chirosree007/Todo-List",
-      live: "https://todoomy.netlify.app"
-   },
-
-
-
-]
-
+  {
+    title: "Serenity Yoga Studio",
+    description:
+      "A modern interactive yoga studio website with calming design, responsive layouts.",
+    tech: "React, Tailwind CSS",
+    image: "/serenity-yoga.jpg",
+    code: "https://github.com/Chirosree007/serenity-yoga-studio",
+    live: "https://serenityy-yoga-studio.netlify.app",
+  },
+];
